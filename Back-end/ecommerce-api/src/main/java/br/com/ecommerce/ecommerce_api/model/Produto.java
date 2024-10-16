e784aabed8803e298a25731e74f0ef4677f22ba6
Nicolas Santos <nicollas.ricardo@hotmail.com>
2024-10-15 21:22:22 -0300

    Implementação da entidade Produto concluida

--- Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/model/Produto.java
+++ Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/model/Produto.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
+//@Table(name = "produtos")
 public class Produto {
 
     @Id
@@ -19,4 +22,108 @@ public class Produto {
     @Column(name = "preco", nullable = false)
     private Double preco;
 
+    @Column(name = "quantidade_em_estoque", nullable = false)
+    private Integer quantidadeEmEstoque;
+
+    @Column(name = "marca", nullable = false)
+    private String marca;
+
+    @Column(name = "categoria", nullable = false)
+    private String categoria;
+
+    @Column(name = "descricao", nullable = false, length = 255)
+    private String descricao;
+
+    @Column(name = "data_cadastro", nullable = false)
+    private LocalDate dataCadastro;
+
+    @Column(name = "ativo")
+    private Boolean ativo;
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidadeEmEstoque=" + quantidadeEmEstoque +
+                ", marca='" + marca + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataCadastro=" + dataCadastro +
+                ", ativo=" + ativo +
+                '}';
+    }
 }
