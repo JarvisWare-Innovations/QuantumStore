6c7945a2dfe846381a021ab58f947ec703a69d2b
Nicolas Santos <nicollas.ricardo@hotmail.com>
2024-10-15 21:07:52 -0300

    Implementando Entidade Produto

--- Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/model/Produto.java
+++ Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/model/Produto.java
@@ -1,10 +1,22 @@
 package br.com.ecommerce.ecommerce_api.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Produto {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "id_produto")
+    private long id;
+
+    @Column(name = "nome_produto", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "preco", nullable = false)
+    private Double preco;
 
 }
