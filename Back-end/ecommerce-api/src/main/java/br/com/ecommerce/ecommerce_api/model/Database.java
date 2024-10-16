70bd0e299baf07c5137a1cbbf1138a2cfe49eb37
Carlos Eduardo da Silva Ribeiro <carlos847@gmail.com>
2024-10-15 22:10:11 -0300

    Adiciona classe Database para simular armazenamento de objetos em memória

---
+++ Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/model/Database.java
@@ -0,0 +1,24 @@
+package br.com.ecommerce.ecommerce_api.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Database {
+
+    private Map<Integer, Object> database = new HashMap<>();
+    private int idSequence = 1;
+
+    public void salvarObjeto(Object obj) {
+        database.put(idSequence++, obj);
+        System.out.println("Objeto salvo com sucesso:" + obj);
+    }
+
+    public Object buscarObjeto(int id) {
+        return database.get(id);
+    }
+
+    public void listarObjetos(){
+        database.forEach((id, obj) -> System.out.println("ID:" + id + " - Objeto:" + obj));
+    }
+
+}
