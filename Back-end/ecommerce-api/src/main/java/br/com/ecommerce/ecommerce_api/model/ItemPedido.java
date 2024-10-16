0922bf06782278d32ba8a0ca9f74f6670a2d3984
Carlos Eduardo da Silva Ribeiro <carlos847@gmail.com>
2024-10-15 21:43:23 -0300

    Implementação da Entidade ItemPedido concluida

---
+++ Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/model/ItemPedido.java
@@ -0,0 +1,62 @@
+package br.com.ecommerce.ecommerce_api.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "tb_itens_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int quantidade;
+    private double precoUnitario;
+
+    public ItemPedido(){}
+
+    public ItemPedido(int quantidade, double precoUnitario){
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    // Getters and Setters
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return this.precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double calcularSubtotal(){
+        return quantidade * precoUnitario;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemPedido{" +
+        "quantidade=" + quantidade + 
+        ", precoUnitario=" + precoUnitario +
+        ", subtotal= " + calcularSubtotal() +
+        '}';
+    }
+}
