2fdf2d763c5e78c051412c319c958d9470a29421
Nicolas Santos <nicollas.ricardo@hotmail.com>
2024-10-16 21:10:20 -0300

    Implementação da classe AdminService e criação de PagamentoRepository

---
+++ Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/service/AdminService.java
@@ -0,0 +1,49 @@
+package br.com.ecommerce.ecommerce_api.service;
+
+import br.com.ecommerce.ecommerce_api.model.Admin;
+import br.com.ecommerce.ecommerce_api.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Nícolas Santos
+ */
+
+@Service
+public class AdminService {
+
+    //    Injeção dependências
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public Admin create(Admin admin) {
+        return adminRepository.save(admin);
+    }
+
+    public List<Admin> obterAdmin() {
+        return adminRepository.findAll();
+    }
+
+    public Admin update(Long id, Admin admin) {
+        if (adminRepository.existsById(id)) {
+            admin.setId_admin(id);
+        } else {
+            throw new RuntimeException("Este Admin não consta em nosso base de dados, Para de viajar e digita certo o bagulho!");
+        }
+
+        return admin;
+    }
+
+    public void delete(Long id) {
+        if (adminRepository.existsById(id)){
+            adminRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Pelo amor de Deus Digite certo essa budega!");
+        }
+    }
+
+
+
+}
