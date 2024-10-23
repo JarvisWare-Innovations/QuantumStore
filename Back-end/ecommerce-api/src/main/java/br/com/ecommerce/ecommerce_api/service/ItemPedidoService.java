a0ad0903978d233a2d572f05a53eb01941d2d03f
Carlos Eduardo da Silva Ribeiro <carlos847@gmail.com>
2024-10-22 21:51:34 -0300

    Remoção do import do PedidoService

--- Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/service/ItemPedidoService.java
+++ Back-end/ecommerce-api/src/main/java/br/com/ecommerce/ecommerce_api/service/ItemPedidoService.java
@@ -2,7 +2,6 @@ package br.com.ecommerce.ecommerce_api.service;
 
 import java.util.List;
 
-import br.com.ecommerce.ecommerce_api.model.Produto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
